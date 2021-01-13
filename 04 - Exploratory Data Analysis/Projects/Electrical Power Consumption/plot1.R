4b40f42a601f8cc4f85b5a386a71ee6017b6cdc0
alexandermfisher <alexandermfisher73@gmail.com>
2021-01-13 16:19:28 +0000

    add code for plots 1,2,3, and 4

---
+++ 04 - Exploratory Data Analysis/Projects/Electrical Power Consumption/plot1.R
@@ -0,0 +1,15 @@
+# load data into workspace:
+dt <- data.table::fread(input = "./data/household_power_consumption.txt", na.strings="?")
+
+# Format Date and Global_active_power cols:
+dt[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+dt[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+# Subset into relevant Dates:
+dt <- subset(dt,(Date >= "2007-02-01") & (Date < "2007-02-03"))
+
+# Plot histogram into png file:
+png("plot1.png", width=480, height=480)
+hist(dt[, Global_active_power], main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
